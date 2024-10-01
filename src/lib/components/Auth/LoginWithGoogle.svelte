1b539e829b1686bb2212fdf417a9e542e70dfd24
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2024-10-01 16:28:55 +0900

    update20241001

---
+++ src/lib/components/Auth/LoginWithGoogle.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {loginWithGoogle} from '$lib/firebase/auth.client';
+  import messagesStore from '$lib/stores/messages.store';
+
+  async function loginGoogle() {
+    try {
+      const user = await loginWithGoogle();
+      console.log(user);
+    } catch (error) {
+      if (error.code === 'auth/popup-closed-by-user') {
+        return;
+      }
+      console.log(error)
+      messagesStore.showError();
+    }
+  }
+</script>
+<div class="row">
+  <div class="col" on:click={loginGoogle}>
+    <button class="btn btn-primary w-100">
+      Login with Google
+    </button>
+  </div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<div class="row">
+  <div class="row">
+    <h1>
+      About Booklovers 
+    </h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequuntur dolorem ullam repellendus nam quam, unde quibusdam aliquam ad nulla totam nisi? Consequuntur neque eos provident tenetur deserunt eveniet molestias.</p>
+  </div>
+</div>
+
+<svelte:head>
+  <title>
+    Booklovers - About
+  </title>
+</svelte:head>
