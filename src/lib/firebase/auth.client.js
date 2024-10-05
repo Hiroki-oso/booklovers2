8155ceceb005a1a26ed26fd15a69373f15a0bc61
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2024-10-05 13:27:32 +0900

    20241005

--- src/lib/firebase/auth.client.js
+++ src/lib/firebase/auth.client.js
@@ -1,4 +1,6 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, 
+  createUserWithEmailAndPassword, signInWithEmailAndPassword} 
+  from 'firebase/auth';
 
 export async function loginWithGoogle() {
   const auth = getAuth();
@@ -8,4 +10,14 @@ export async function loginWithGoogle() {
 
 export async function logout() {
   await signOut(getAuth()); 
+}
+
+export async function registerWithEmailandPassword(email, password) {
+  const userCredential = await  createUserWithEmailAndPassword(getAuth(), email, password)
+  return userCredential.user;
+}
+
+export async function loginWithEmailandPassword(email, password) {
+  const userCredential = await signInWithEmailAndPassword(getAuth(), email, password);
+  return userCredential.user;
 }
